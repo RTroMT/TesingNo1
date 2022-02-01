b98d2cdbbeffed38decc4558d322f4b5bfdb3ae5
MT <n.minhtri1105@gmail.com>
2022-02-01 20:18:12 +0700

    Adding variables

--- PTBacII.c
+++ PTBacII.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 
-int calculation(int a, int b, double delta);
+int calculation(int a, int b, int c, double delta);
 
 int main()
 {
-    int a, b;
+    int a, b, c;
     double delta;
+    printf("Nhap cac co so.\n");
+    printf("Nhap co so 1: ");
+    scanf(&a);
+    printf("\nNhap co so 2: ");
+    scanf(&b);
+    printf("\nNhap co so 3: ");
+    scanf(&c);
+    delta = b * b - 4 * a * c;
 }
